b2f67aa4d09c2b9d3f4cf6ac58d1cf1a9dffbdd2
ershawarma <samuel.ssrc@gmail.com>
2022-08-25 19:38:31 -0500

    aug 25

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import Particooles from './Components/Particooles/Particooles';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import Clarifai from 'clarifai';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
+import Rank from './Components/Rank/Rank';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import { Component } from 'react';
 
-function App() {
+
+const app = new Clarifai.App({
+  apiKey: 'b362b100722d41688391ff34255a9785'
+});
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+    // console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    // console.log('click');
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        // '53e1df302c079b3db8a0a36033ed2d15'
+        // 'https://samples.clarifai.com/face-det.jpg'
+        this.state.input)
+      .then(
+      function(response){
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+          // response.outputs[0].data.regions[0].region_info.bounding_box
+      },
+      function(err){
+        // there was an error
+      }
+    );
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particooles/>
+      <Navigation />
+      <Logo />
+      <Rank/>
+      <ImageLinkForm 
+        onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}/>
+      <FaceRecognition 
+      imageUrl={this.state.imageUrl}
+      />
     </div>
   );
+ }
 }
-
 export default App;
---
+++ src/Components/Particooles/Particooles.js
@@ -0,0 +1,71 @@
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+const Particooles = () => {
+  const particlesInit = async (main) => {
+    // console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(main);
+  };
+
+  const particlesLoaded = (container) => {
+    // console.log(container);
+  };
+  return (
+    <Particles className="particles"
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fpsLimit: 120,
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 0.8,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+};
+
+export default Particooles;
